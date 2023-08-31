861c3d17df8e7dfe6b04dcc44535b91d04a0a74a
Andrew Cring <amcring1@gmail.com>
2023-08-30 21:49:56 -0700

    Working on the basic routing structure

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
 if __name__ == '__main__':
     app.run(debug=True)
