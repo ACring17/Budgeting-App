a24ecee99bbe98fd53267ac555ac7d4365b33924
Andrew Cring <amcring1@gmail.com>
2023-09-28 11:56:24 -0700

    Working on setting up the db

--- budget.py
+++ budget.py
@@ -6,3 +6,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField#,BooleanField,DateTimeField,RadioField,SelectField,TextAreaField,TextField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+# Configuring the db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Connecting the SQL
+db = SQLAlchemy(app)
+
+Migrate(app,db)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
