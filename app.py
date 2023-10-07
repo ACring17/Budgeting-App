e4fd92bab8cbc7bf07f7b192a0597551ce2856eb
Andrew Cring <amcring1@gmail.com>
2023-10-07 09:14:17 -0700

    Trimming repeat code from app.py

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-from flask import Flask, render_template, request
+from Budgeting-App import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
-
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__=='__main__':
+    app.run(debug=True) 
